94a38e5ff8f1f7a394ba3b1a4ac4d32e9b0e4a9c
liyang <235938019@qq.com>
2019-09-18 08:11:52 +0000

    liyang

---
+++ add_sdc.sh
@@ -0,0 +1,11 @@
+scli --login --username admin --password liyang@008
+scli --query_all_sdc
+read -p "pls input sdc ip: " ip
+read -p "pls input mdm vip: " vip
+ssh $ip rpm -ivh /root/scaleio/EMC-ScaleIO-lia-3.0-200.104.el7.x86_64.rpm
+ssh $ip rpm -ivh /root/scaleio/EMC-ScaleIO-sdc-3.0-200.104.el7.x86_64.rpm
+echo "mdm $vip" >> /etc/emc/scaleio/drv_cfg.txt
+ssh $ip systemctl restart scini
+sleep 5
+scli --add_sdc --sdc_ip $ip
+scli --query_all_sdc
