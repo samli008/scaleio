449c30cb1e4e81aef19bd5106d5d5e733226f2fc
liyang <235938019@qq.com>
2019-09-19 06:26:43 +0000

    liyang

--- add_vol.sh
+++ add_vol.sh
@@ -7,10 +7,10 @@ read -p "pls input vol name: " vol
 read -p "pls input vol size: " size
 read -p "pls input pool name: " pool
 scli --add_volume --protection_domain_name $pool --storage_pool_name $pool --size_gb $size --volume_name $vol
-scli --query_all_vol |grep -i not
+scli --query_all_vol |grep -i unmapped
 
 elif [ $add == 0 ];then
-scli --query_all_vol |grep -i not
+scli --query_all_vol |grep -i unmapped
 read -p "pls input vol name: " vol
 scli --remove_volume --volume_name $vol 
 
