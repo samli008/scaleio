6ae68a8d394952353756c755d40f3de8c56bd76d
liyang <235938019@qq.com>
2020-07-30 21:45:25 -0400

    liyang

--- gfs2.sh
+++ gfs2.sh
@@ -16,10 +16,10 @@ pcs resource create vip IPaddr2 ip=$vip cidr_netmask=24 op monitor interval=20s 
 pcs resource create dlm ocf:pacemaker:controld op monitor interval=30s on-fail=fence clone interleave=true ordered=true
 
 # create gfs2
-mkfs.gfs2 -j 2 -p lock_dlm -t cluster1:gfs1 /dev/drbd0
+mkfs.gfs2 -j 2 -p lock_dlm -t cluster1:gfs1 /dev/scinia
 
 # create fs-clone
-pcs resource create fs ocf:heartbeat:Filesystem device="/dev/drbd0" directory="/kvm" fstype="gfs2" --clone
+pcs resource create fs ocf:heartbeat:Filesystem device="/dev/scinia" directory="/kvm" fstype="gfs2" --clone
 
 # create HA-vm
 pcs resource create c7-1 VirtualDomain hypervisor="qemu:///system" config="/kvm/c7-1.xml" migration_transport=ssh --group group1
