a18e1c5f3ecda8dbe1d785d9c3642bd8e331628b
liyang <235938019@qq.com>
2020-07-30 21:41:46 -0400

    liyang

--- gfs2.sh
+++ gfs2.sh
@@ -1,10 +1,16 @@
-# dual primary drbd 
-read -p "pls input drbd device [drbd0]: " drbd
-pcs cluster cib drbd_cfg
-pcs -f drbd_cfg resource create data ocf:linbit:drbd drbd_resource=$drbd op monitor interval=60s
-pcs -f drbd_cfg resource master dataClone data master-max=2 master-node-max=1 clone-max=2 clone-node-max=1 notify=true
-pcs -f drbd_cfg resource show
-pcs cluster cib-push drbd_cfg
+# scsi fence device
+echo "pls use ll /dev/disk/by-id confirm stonith disk device"
+read -p "pls intput node1 hostname: " pcs1
+read -p "pls intput node2 hostname: " pcs2
+read -p "pls input stonith disk [sdb]: " sd
+read -p "pls input nfs vip [192.168.20.174]: " vip
+
+a=`ls -l /dev/disk/by-id | grep $sd |awk NR==2'{print $9}'`
+pcs stonith create scsi-shooter fence_scsi pcmk_host_list="$pcs1 $pcs2" devices=/dev/disk/by-id/$a meta provides=unfencing
+pcs property set no-quorum-policy=freeze
+pcs stonith show scsi-shooter
+
+pcs resource create vip IPaddr2 ip=$vip cidr_netmask=24 op monitor interval=20s OCF_CHECK_LEVEL=10 on-fail=fence --group group1
 
 # create dlm-clone
 pcs resource create dlm ocf:pacemaker:controld op monitor interval=30s on-fail=fence clone interleave=true ordered=true
